46bbc459958974e3c1c6e471ddf725085390d562
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-01 22:40:17 +0800

    pseudocode

---
+++ print_without_using_index.py
@@ -0,0 +1,10 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# ask the user for input
+# ask the user what character to find
+# initialize position as -1 if the character is not found
+# initialize index as 0
+# check each character if it match the character to find
+# if no, increment index variable until character is found
+# if yes, set the position to the current index then break
+# print the position
